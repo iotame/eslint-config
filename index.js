7b38146286c2596f8cd42691f2f7b6ab38cbed34
Christopher Mühl <christopher@padarom.xyz>
2020-03-15 00:27:20 +0100

    Add standard

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 module.exports = {
+  extends: 'standard',
   rules: {
+    'indent': ['error', 2],
+    'comma-dangle': ['error', {
+      'arrays': 'always-multiline',
+      'objects': 'always-multiline',
+      'imports': 'never',
+      'exports': 'never',
+      'functions': 'never',
+    }]
   }
 }
